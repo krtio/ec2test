8138debf9da4a24cc22c657f11b807ea3c61ae84
krtio <minwoo0612k@naver.com>
2024-09-20 18:46:27 +0900

    testqweqweqweqwe

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo ">>> .html 파일 복사" >> /home/ubuntu/deploy.log
-cp /home/ubuntu/source/*.html /home/ubuntu/
+echo ">>> .html 파일 복사" >> /home/ubuntu/aaa/deploy.log
+cp /home/ubuntu/source/*.html /home/ubuntu/aaa
